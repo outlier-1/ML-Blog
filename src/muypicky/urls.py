b38cfd868207aeb384506172a357ea20821d29f9
outlier-1 <omermirac@gmail.com>
2017-08-26 17:40:41 +0300

    Changes on models

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from restaurants.views import HomeView #Because we have extra context in that
+from restaurants.views import restaurants_listview #Because we have extra context in that
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view()),
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^restaurants/$', restaurants_listview),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html")),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,30 +1,13 @@
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import TemplateView
-import random
+from django.shortcuts import render
+from .models import RestaurantLocation
 
 # Create your views here
 
-
-class HomeView(TemplateView):
-    template_name = "home.html"
-
-    def get_context_data(self, *args, **kwargs):
-        num=None
-        some_list= [random.randint(0,10000), random.randint(0,10000), random.randint(0,10000)]
-        conditional_bool_item= True
-        if conditional_bool_item:
-            num=random.randint(0,10000)
-
-        context={ "num":num,
-        "some_list":some_list
-        }
-        return context
-
-
-class AboutView(TemplateView):
-    template_name = "about.html"
-
-
-class ContactView(TemplateView):
-    template_name = "contact.html"
+def restaurants_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    querySet=RestaurantLocation.objects.all()
+    context = {
+        "object_list" : querySet,
+    }
+    return render(request, template_name, context)
